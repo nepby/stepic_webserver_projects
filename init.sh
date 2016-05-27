aa7ddf260a1a4c2daa70e697ee4e9af073c372fc
nepby <nepby@outlook.com>
2016-05-27 11:43:14 +0300

    вариант из предыдущего задания

--- init.sh
+++ init.sh
@@ -1,9 +1,7 @@
-#mkdir /home/box/web/logs 2> /dev/null
-
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf 
-sudo /etc/init.d/nginx restart 
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test 
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-
-#kill -HUP /home/box/web/etc/gunicorn.pid 2> /dev/null
-#gunicorn -c etc/gunicorn.conf /etc/gunicorn.d/django.wsgi
+cd /home/box/web/
+sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:app
