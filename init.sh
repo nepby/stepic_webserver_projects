ebd22ac87c08831dc1b5796364baadf5478a2fd6
nepby <nepby@outlook.com>
2016-05-27 11:01:22 +0300

    Вариант от преподователя

--- init.sh
+++ init.sh
@@ -3,10 +3,9 @@ git config --global user.name luixo
 git config --global user.email lexx92@mail.ru
 mkdir /home/box/web/logs 2> /dev/null
 
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/django.wsgi /etc/gunicorn.d/django.wsgi
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/init.d/gunicorn.conf
+sudo ﻿ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf 
+sudo /etc/init.d/nginx restart 
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test 
 sudo /etc/init.d/gunicorn restart
 
 #kill -HUP /home/box/web/etc/gunicorn.pid 2> /dev/null
