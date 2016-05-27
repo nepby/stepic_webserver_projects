706a097ac32f4dc9435fe2b78b4f8f59c81c3548
nepby <nepby@outlook.com>
2016-05-27 11:45:25 +0300

    забыл добавить

--- init.sh
+++ init.sh
@@ -5,3 +5,4 @@ sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 cd /home/box/web/
 sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:app
+gunicorn -c django.py wsgi --daemon
